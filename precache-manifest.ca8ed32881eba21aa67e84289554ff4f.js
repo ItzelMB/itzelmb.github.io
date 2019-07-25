1fe698c7435fe94e318cb07ade6f5e28b914f018
ItzelMB <it.mobec@gmail.com>
2019-07-25 11:15:26 -0500

    Updates

---
+++ precache-manifest.ca8ed32881eba21aa67e84289554ff4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bcdd29506ffd18537c391b0f32670b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5abd2cbfaf9481d2437d",
+    "url": "/static/css/main.69745b7e.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5abd2cbfaf9481d2437d",
+    "url": "/static/js/main.eb9104ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffb35d0a406e98a3b14cfd01a819e126",
+    "url": "/static/media/in3.ffb35d0a.png"
+  }
+]);
