803aca5236bcdf5711f64f50d85dbd6449e30ce9
ItzelMB <it.mobec@gmail.com>
2019-07-25 10:48:16 -0500

    Add new styles in home secc

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,9 @@ import './home.css';
 
 const Home = () => (
     <section class="home bg-fixed">
-        <div class="pt-48">
-            <p className="name">Itzel Mb</p>
-            <p className="title mt-5">I AM A FRONT-END DEVELOPER & GRAPHIC DESIGNER</p>
+        <div class="container mx-auto px-12 md:px-24">
+            <p className="name text-center pt-40 md:pt-64">Itzel Mb</p>
+            <p className="title mt-5 text-center">I AM A FRONT-END DEVELOPER & GRAPHIC DESIGNER</p>
         </div>
     </section>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   theme: {
-    screens: {
+    /*screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-    },
+    },*/
     extend: {}
   },
   variants: {},
   plugins: [],
-  modules: {
+  /*modules: {
     margin: false,
     negativeMargin: false,
     padding: false,
-  }
+  }*/
 }
